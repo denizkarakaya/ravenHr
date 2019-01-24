23c0266eda49665fce78dff479dbe86cd14a5eb9
dkarakaya <deniz.karakaya@tav.aero>
2019-01-24 17:14:14 +0300

    Last Changes

--- src/main/java/com/raven/hr/controller/WelcomeController.java
+++ src/main/java/com/raven/hr/controller/WelcomeController.java
@@ -1,28 +1,39 @@
 package com.raven.hr.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.raven.hr.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 @Controller
 public class WelcomeController {
 
-    private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        System.out.println("Home Page Requested, locale = " + locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
-    @GetMapping("/bordro_war")
-    public String index(Model model) {
-        logger.debug("Welcome to mkyong.com...");
-        model.addAttribute("msg", getMessage());
-        model.addAttribute("today", new Date());
-        return "index";
+        String formattedDate = dateFormat.format(date);
 
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
     }
 
-    private String getMessage() {
-        return "Hello World";
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String user(@Validated User user, Model model) {
+        System.out.println("User Page Requested");
+        model.addAttribute("userName", user.getUserName());
+        return "user";
     }
 }
